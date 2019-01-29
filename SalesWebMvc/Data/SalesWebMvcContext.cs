b82d6d4b8d3bbd13b2a8c68ef54aab79b59381f3
dilonghi <diego@ibservi.com.br>
2019-01-29 20:05:07 -0200

    Criação das Entidades Salesrecord e seller e novo migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -10,5 +10,7 @@ namespace SalesWebMvc.Models
         }
 
         public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
---
+++ SalesWebMvc/Models/SellesRecord.cs
@@ -0,0 +1,28 @@
+﻿using SalesWebMvc.Models.Enums;
+using System;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public SalesRecord()
+        {
+
+        }
+
+        public SalesRecord(int id, DateTime date, double ammount, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Ammount = ammount;
+            Status = status;
+            Seller = seller;
+        }
+
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Ammount { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+    }
+}
