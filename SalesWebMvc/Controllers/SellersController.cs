85a95e2b244b6ad2c2647c8d2722fb0952f714c9
dilonghi <diego@ibservi.com.br>
2019-01-30 19:35:54 -0200

    Implementando seller insert

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -21,5 +22,26 @@ namespace SalesWebMvc.Controllers
             var sellers = _sellerService.FindAll();
             return View(sellers);
         }
+
+
+        public IActionResult Create()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            if(ModelState.IsValid)
+            {
+                _sellerService.Insert(seller);
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
     }
 }
